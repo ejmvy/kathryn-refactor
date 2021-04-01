189b92346f9fe77f64dff2e553fd67d4f9eced92
EJ McVey <ej.mcvey@geopal-solutions.com>
2021-04-01 21:29:41 +0100

    collection section comps new css

---
+++ src/components/Designs/LeftCollectionComponent.vue
@@ -0,0 +1,60 @@
+<template>
+  <div
+    class="flex flex-col justify-center items-center my-5 md:flex-row w-11/12 md:w-3/4"
+  >
+    <div class="h-96 w-full md:w-8/12 z-0 shadow-xl">
+      <img
+        class="w-full h-full"
+        :src="require(`../../assets/${displayCollection.img}`)"
+      />
+      <!-- <img class="reasonImage" src="../../assets/latestDesigns/dish1.jpeg" /> -->
+    </div>
+    <div
+      class="flex flex-col items-center text-center relative -top-10 md:top-0 w-3/4 md:-left-3 p-8 md:flex-1 md:w-40 z-10 bg-white shadow-2xl"
+    >
+      <div class="lineTop">
+        <div class="line1 w-48 md:w-36"></div>
+        <div class="line2 w-24 mt-3"></div>
+        <div class="dot mb-8 mt-3"></div>
+      </div>
+      <div class="text-sm">
+        <h2 class="font-bold pb-5 md:pb-10 uppercase">
+          {{ displayCollection.title }}
+        </h2>
+        <p>
+          {{
+            getProductText
+              ? displayCollection.shortText
+              : displayCollection.longText
+          }}
+        </p>
+        <!-- <router-link
+          :to="{ name: 'ShowCollection', params: { category: collection } }"
+        >
+          <button class="btn">SHOP NOW</button>
+        </router-link> -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["displayCollection", "collection"],
+  data() {
+    return {
+      windowWidth: window.innerWidth,
+    };
+  },
+  mounted() {
+    window.addEventListener("rezise", () => {
+      this.windowWidth = window.innerWidth;
+    });
+  },
+  computed: {
+    getProductText() {
+      return this.windowWidth <= 600;
+    },
+  },
+};
+</script>
---
+++ src/components/LandingPage/Collection.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="flex flex-col items-center w-full pb-16 bg-gray-100 mt-20">
+    <div class="w-full flex flex-col items-center py-16">
+      <TitleDesign class="mb-20" :title="titleText"></TitleDesign>
+      <LeftCollectionComponent
+        :displayCollection="dishCollection"
+        :collection="dishes"
+      ></LeftCollectionComponent>
+      <RightCollectionComponent
+        :displayCollection="mugCollection"
+        :collection="mugs"
+      ></RightCollectionComponent>
+      <LeftCollectionComponent
+        :displayCollection="bowlCollection"
+        :collection="bowls"
+      ></LeftCollectionComponent>
+      <RightCollectionComponent
+        :displayCollection="miscCollection"
+        :collection="misc"
+      ></RightCollectionComponent>
+    </div>
+  </div>
+</template>
+
+<script>
+import TitleDesign from "../Designs/TitleDesign.vue";
+import LeftCollectionComponent from "../Designs/LeftCollectionComponent.vue";
+import RightCollectionComponent from "../Designs/RightCollectionComponent.vue";
+export default {
+  data() {
+    return {
+      titleText: "The Collection",
+      dishes: "dishes",
+      mugs: "mugs",
+      bowls: "bowls",
+      misc: "misc",
+      dishCollection: {
+        title: "Dishes",
+        longText:
+          "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour.",
+        shortText:
+          "There are many variations of passages of Lorem Ipsum available.",
+        img: "latestDesigns/dish1.jpeg",
+      },
+      bowlCollection: {
+        title: "Bowls",
+        longText:
+          "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour.",
+        shortText:
+          "There are many variations of passages of Lorem Ipsum available.",
+        img: "latestDesigns/bowl1.jpeg",
+      },
+      mugCollection: {
+        title: "Mugs",
+        longText:
+          "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour.",
+        shortText:
+          "There are many variations of passages of Lorem Ipsum available.",
+        img: "latestDesigns/mug3.jpeg",
+      },
+      miscCollection: {
+        title: "Misc",
+        longText:
+          "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour.",
+        shortText:
+          "There are many variations of passages of Lorem Ipsum available.",
+        img: "latestDesigns/misc1.jpeg",
+      },
+    };
+  },
+  components: {
+    TitleDesign,
+    LeftCollectionComponent,
+    RightCollectionComponent,
+  },
+};
+</script>
+
+<style scoped>
+#collectionArea {
+  background: #f5f5f5;
+  padding-bottom: 30px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.collectionSection {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-bottom: 50px;
+  /* border: 2px solid red; */
+}
+
+@media only screen and (min-width: 1200px) {
+  .collectionSection {
+    width: 80%;
+  }
+}
+
+@media screen and (max-width: 600px) {
+  .collectionArea {
+    padding-bottom: 5px;
+  }
+  .collectionSection {
+    padding-bottom: 0;
+  }
+}
+</style>
