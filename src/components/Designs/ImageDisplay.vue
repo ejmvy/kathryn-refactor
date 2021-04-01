869231d158af748a591a5cdff23d057621e34755
EJ McVey <ej.mcvey@geopal-solutions.com>
2021-04-01 21:30:02 +0100

    new comps with css

---
+++ src/components/Designs/ImageDisplay.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex flex-wrap justify-center mt-10 w-full">
+    <div v-for="img in images" :key="img">
+      <img
+        class="my-8 sm:mx-8 h-60 w-80"
+        :src="require(`../../assets/${img}`)"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["images"],
+};
+</script>
---
+++ src/components/Designs/TitleDesign.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex items-center w-11/12 justify-center mt-5 mb-4">
+    <div class="w-full bg-green-dark h-0.5"></div>
+    <div class="titleText w-4/6 text-md uppercase">{{ title }}</div>
+    <div class="w-full bg-green-dark h-0.5"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["title"],
+};
+</script>
+
+<style scoped>
+.titleText {
+  font-family: "Nova Mono", monospace;
+}
+</style>
