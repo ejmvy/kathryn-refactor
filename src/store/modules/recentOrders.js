cca330e368ceff5a7e712704c66f4f512b764731
EJ McVey <ej.mcvey@geopal-solutions.com>
2021-05-05 20:29:09 +0100

    new admin orders store vuex

---
+++ src/store/modules/recentOrders.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+export default {
+  namespaced: true,
+  state() {
+    return {
+      orders: [],
+    };
+  },
+  mutations: {
+    setOrders(state, orderData) {
+      state.orders = orderData;
+    },
+    updateDeliveredStatus(state, orderId) {
+      try {
+        const order = state.orders.find((order) => order._id === orderId);
+        if (order) order.isDelivered = true;
+        console.log("updated order: ");
+        console.log(order);
+      } catch (e) {
+        console.log(`error: ${e}`);
+      }
+    },
+  },
+  actions: {
+    async callOrdersAPI({ commit }) {
+      await axios
+        .get("http://localhost:3000/api/orders/recent")
+        .then((result) => {
+          commit("setOrders", result.data);
+        });
+    },
+    updateDeliveredStatus(context, orderId) {
+      context.commit("updateDeliveredStatus", orderId);
+    },
+  },
+  getters: {
+    getOrders: (state) => state.orders,
+  },
+};
