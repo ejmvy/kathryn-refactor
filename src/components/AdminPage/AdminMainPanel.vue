fc55ff94cab4b85b2158e190759a9108e7f1a660
EJ McVey <ej.mcvey@geopal-solutions.com>
2021-05-05 20:31:00 +0100

    updated admin panel with transitions

--- src/components/AdminPage/AdminMainPanel.vue
+++ src/components/AdminPage/AdminMainPanel.vue
@@ -3,31 +3,31 @@
     <AdminLogin v-if="!userLoggedIn"></AdminLogin>
 
     <div v-if="userLoggedIn">
-      <AdminHeader></AdminHeader>
+      <AdminHeader @logout="logoutUser"></AdminHeader>
 
       <div class="w-2/3 m-auto my-10">
         <div class="flex justify-between items-center">
           <img
-            @click="changeWindow(0)"
+            @click="changeWindow('recentOrders')"
             class="w-10 h-10 cursor-pointer"
             src="https://i.ibb.co/W3kjX3s/file-green.png"
           />
           <div class="line"></div>
 
           <img
-            @click="changeWindow(1)"
+            @click="changeWindow('statsPanel')"
             class="w-10 h-10 cursor-pointer"
             src="https://i.ibb.co/HdwZh2S/graph.png"
           />
           <div class="line"></div>
           <img
-            @click="changeWindow(2)"
+            @click="changeWindow('orderHistory')"
             class="w-10 h-10 cursor-pointer"
             src="https://i.ibb.co/DRD73Z5/list-green.png"
           />
           <div class="line"></div>
           <img
-            @click="changeWindow(3)"
+            @click="changeWindow('editPanel')"
             class="w-10 h-10 cursor-pointer"
             src="https://i.ibb.co/N6vxTqj/pencil-green.png"
           />
@@ -35,14 +35,21 @@
       </div>
 
       <div class="pt-8 border-t border-gray-200">
-        <div class="adminArea">
-          <RecentOrderPanel
-            class="panel"
-            v-if="currentIndex === 0"
-            key="1"
-          ></RecentOrderPanel>
-        </div>
+        <transition name="component-fade" mode="out-in">
+          <keep-alive>
+            <component :is="view"></component>
+          </keep-alive>
+        </transition>
       </div>
+
+      <transition name="slide-up">
+        <ConfirmPopup
+          v-if="showPopup"
+          :popupData="popupMessage"
+          @closePopup="showPopup = false"
+          @confirmAction="saveAction"
+        ></ConfirmPopup>
+      </transition>
     </div>
   </div>
 </template>
@@ -52,8 +59,9 @@ import AdminLogin from "../AdminPage/AdminLogin.vue";
 import AdminHeader from "../AdminPage/AdminHeader.vue";
 // import AdminConfigPangel from "../AdminPage/AdminConfigPanel.vue";
 import RecentOrderPanel from "../AdminPage/RecentOrderPanel.vue";
-// import OrderHistoryPanel from "../AdminPage/OrderHistoryPanel.vue";
-// import StatsPanel from "../AdminPage/StatsPanel.vue";
+import OrderHistoryPanel from "../AdminPage/OrderHistoryPanel.vue";
+import StatsPanel from "../AdminPage/StatsPanel.vue";
+import ConfirmPopup from "../Designs/ConfirmPopup.vue";
 
 export default {
   data() {
@@ -63,22 +71,30 @@ export default {
       openHistoryPanel: false,
       currentIndex: 0,
       back: false,
+      view: "recentOrders",
+      showPopup: false,
+      popupMessage: {
+        title: "",
+        message: "",
+      },
     };
   },
   methods: {
-    changeWindow(newIdx) {
-      if (this.currentIndex < newIdx) {
-        // console.log("HIGHER");
-        this.back = false;
-        // console.log('BACK:' + this.back );
-        this.currentIndex = newIdx;
-      } else if (this.currentIndex > newIdx) {
-        // console.log("LOWER");
-        this.back = true;
-
-        this.currentIndex = newIdx;
-      }
-      // console.log(`CURRENT idx: ${this.currentIndex}`);
+    changeWindow(tab) {
+      this.view = tab;
+    },
+    logoutUser() {
+      this.popupMessage.title = "Logout ?";
+      this.popupMessage.message = "Are you sure you wish to leave?";
+      this.showPopup = true;
+    },
+    saveAction() {
+      this.showPopup = false;
+      this.$store.dispatch("logout");
+      this.emitter.emit("showNotification", {
+        state: true,
+        message: "Bye for now",
+      });
     },
   },
   computed: {
@@ -100,11 +116,11 @@ export default {
   components: {
     AdminLogin,
     AdminHeader,
-
+    ConfirmPopup,
     // AdminConfigPangel,
-    RecentOrderPanel,
-    // OrderHistoryPanel,
-    // StatsPanel,
+    recentOrders: RecentOrderPanel,
+    statsPanel: StatsPanel,
+    orderHistory: OrderHistoryPanel,
   },
 };
 </script>
@@ -115,5 +131,14 @@ export default {
   height: 3px;
   background: rgba(32, 72, 88, 0.7);
   border-radius: 5px;
+}
+
+.component-fade-enter-active,
+.component-fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+.component-fade-enter, .component-fade-leave-to
+/* .component-fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
 }
 </style>
