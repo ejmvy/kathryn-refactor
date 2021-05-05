c79db247cb8c365a3c589a1167aeffc369b15863
EJ McVey <ej.mcvey@geopal-solutions.com>
2021-05-05 20:30:03 +0100

    confirm popup styling

--- src/components/Designs/ConfirmPopup.vue
+++ src/components/Designs/ConfirmPopup.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="w-screen h-screen flex justify-center">
-    <div class="popupArea fixed w-1/2 bg-white shadow-xl m-auto">
+  <div class="w-screen h-screen fixed top-0 left-0 flex justify-center">
+    <div class="popupArea w-1/2 bg-white shadow-xl m-auto">
       <div
         class="w-full py-3 uppercase text-left pl-5 text-xs bg-green-dark text-white font-bold"
       >
         {{ popupData.title }}
       </div>
-      <div class="text-xs px-4 py-10 text-left text-gray-500">
+      <div class="text-sm px-4 py-10 text-left text-gray-500">
         {{ popupData.message }}
       </div>
       <div class="flex justify-between border-t border-gray-400 py-3 px-2">
@@ -35,7 +35,6 @@ export default {
 
 <style scoped>
 .popupArea {
-  top: 40%;
-  margin: 0 auto;
+  min-height: 100px;
 }
 </style>
