01df1ea73fa07fd2eb31ab0c4936d6af9a7c19df
EJ McVey <ej.mcvey@geopal-solutions.com>
2021-05-08 16:47:14 +0100

    changed title font

--- src/components/Designs/TitleDesign.vue
+++ src/components/Designs/TitleDesign.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class="flex items-center w-11/12 justify-center mt-5 mb-4">
-    <div class="w-full bg-green-dark h-0.5"></div>
-    <div class="titleText w-4/6 text-md uppercase">{{ title }}</div>
-    <div class="w-full bg-green-dark h-0.5"></div>
+  <div class="flex items-center w-11/12 justify-center mt-5 mb-7">
+    <div class="w-1/2 sm:w-full bg-green-light h-0.5"></div>
+    <div class="w-4/6 text-sm uppercase text-green-dark">
+      {{ title }}
+    </div>
+    <div class="w-1/2 sm:w-full bg-green-light h-0.5"></div>
   </div>
 </template>
 
@@ -12,8 +14,4 @@ export default {
 };
 </script>
 
-<style scoped>
-.titleText {
-  font-family: "Nova Mono", monospace;
-}
-</style>
+<style scoped></style>
