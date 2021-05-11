ef7ca8e3634f3c9ee380ad28c04068583806a93c
EJ McVey <ej.mcvey@geopal-solutions.com>
2021-05-11 20:50:20 +0100

    header revamp - new style

--- src/components/LandingPage/Header.vue
+++ src/components/LandingPage/Header.vue
@@ -1,99 +1,90 @@
 <template>
-  <header class="absolute bg-green-light w-full">
-    <div class="flex py-3 px-5 md:px-10 justify-between items-center shadow-xl">
-      <router-link class="logo text-3xl text-white" to="/">{{
-        mobileWidth ? shortTitle : longTitle
-      }}</router-link>
-      <nav class="hidden items-center md:flex">
-        <li class="li-class">
-          <router-link to="/">Home</router-link>
-        </li>
-        <li class="li-class">
-          <router-link to="/shop">Shop</router-link>
-        </li>
-        <li class="li-class">
-          <router-link to="/contact">Contact</router-link>
-        </li>
-        <li class="li-class">
-          <a href="#">Gallery</a>
-        </li>
-        <div class="li-class">
-          <router-link :to="checkLoggedIn ? '/userprofile' : '/login'">
-            <img class="w-5 h-5" src="https://i.ibb.co/pn8FWKn/userIcon.png" />
-          </router-link>
-        </div>
-        <div class="li-class cursor-pointer">
-          <div class="flex hover:no-underline">
-            <img
-              class="w-6 h-6"
-              src="https://i.ibb.co/vzdx4Vj/cart.png"
-              @click="openCart()"
-            />
-            <div
-              v-if="getCartLength"
-              class="w-5 h-5 text-xs flex items-center justify-center bg-white rounded-full text-green-light font-bold relative -top-2"
-            >
-              {{ getCartLength }}
-            </div>
-          </div>
-        </div>
-      </nav>
+  <header
+    class="w-full flex justify-between items-center py-3 px-7"
+    :class="{ 'shadow-lg': addShadow }"
+  >
+    <router-link to="/" :class="{ navLogo: hamburgerOpen }">
+      <p
+        class="logo text-green-light text-4xl sm:text-5xl"
+        :class="{ navLogo: hamburgerOpen }"
+      >
+        KC
+      </p>
+    </router-link>
 
-      <!-- MOBILE NAV -->
-      <nav class="flex md:hidden items-center">
-        <div class="mx-4">
-          <img class="w-4 h-4" src="https://i.ibb.co/DGCt4tJ/hamburger.png" />
-        </div>
-        <div class="flex md:hidden items-center">
-          <router-link :to="checkLoggedIn ? '/userprofile' : '/login'">
-            <img
-              class="w-5 h-5 middleLink"
-              src="https://i.ibb.co/pn8FWKn/userIcon.png"
-            />
+    <div class="flex">
+      <svg
+        @click="openCart()"
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-8 w-8 cartIcon cursor-pointer"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="#627F8A"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
+        />
+      </svg>
+
+      <div
+        class="hamburger"
+        @click="openNavPanel"
+        :class="hamburgerOpen ? 'hamburgerOpen' : ''"
+      >
+        <div class="hamburger-line hamburger-line-first"></div>
+        <div class="hamburger-line hamburger-line-middle"></div>
+        <div class="hamburger-line hamburger-line-last"></div>
+      </div>
+
+      <transition name="fall-down">
+        <nav
+          v-if="openNav"
+          class="absolute top-0 left-0 w-screen h-screen flex flex-col items-center bg-black-dark opacity-80 leading-5 text-2xl text-white pt-32"
+        >
+          <router-link to="/">
+            <li>Home</li>
           </router-link>
+          <router-link to="/shop">
+            <li>Shop</li>
+          </router-link>
+          <router-link :to="checkLoggedIn ? '/userprofile' : '/login'">
+            <li>My Account</li>
+          </router-link>
+          <router-link to="#">
+            <li>Gallery</li>
+          </router-link>
+          <router-link to="/contact">
+            <li>Contact</li>
+          </router-link>
+        </nav>
+      </transition>
+      <transition name="slide">
+        <div
+          v-if="cartOpen"
+          class="cartImport absolute top-0 right-0 w-screen h-screen"
+        >
+          <CartPopup @closePopup="openCart()"></CartPopup>
         </div>
-        <div class="ml-4">
-          <div class="flex cursor-pointer">
-            <img
-              class="w-6 h-6"
-              src="https://i.ibb.co/vzdx4Vj/cart.png"
-              @click="openCart()"
-            />
-            <div
-              v-if="getCartLength"
-              class="w-5 h-5 text-xs flex items-center justify-center bg-white rounded-full text-green font-bold relative -top-2"
-            >
-              {{ getCartLength }}
-            </div>
-          </div>
-        </div>
-      </nav>
+      </transition>
     </div>
   </header>
-
-  <transition name="slide">
-    <div v-if="cartOpen" class="cartImport">
-      <CartPopup @closePopup="openCart()"></CartPopup>
-    </div>
-  </transition>
 </template>
 
 <script>
 import CartPopup from "../../views/CartPopup.vue";
 export default {
+  props: ["addShadow"],
   data() {
     return {
       cartOpen: false,
-      windowWidth: window.innerWidth,
-      longTitle: "Ceramics By Kathryn",
-      shortTitle: "KC",
+      openNav: false,
+      hamburgerOpen: false,
     };
   },
-  mounted() {
-    window.addEventListener("rezise", () => {
-      this.windowWidth = window.innerWidth;
-    });
-  },
+
   methods: {
     openCart() {
       this.cartOpen = !this.cartOpen;
@@ -101,11 +92,12 @@ export default {
     showUser() {
       // console.log(`user logged: `, this.$store.state.user);
     },
+    openNavPanel() {
+      this.openNav = !this.openNav;
+      this.hamburgerOpen = !this.hamburgerOpen;
+    },
   },
   computed: {
-    mobileWidth() {
-      return this.windowWidth <= 800;
-    },
     checkLoggedIn() {
       console.log(`LOGGED IN: ${this.$store.getters.isAuthenticated}`);
       return this.$store.getters.isAuthenticated;
@@ -120,28 +112,47 @@ export default {
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-li {
+nav {
+  transition: all 0.5s ease-in-out;
+  z-index: 3;
+}
+nav li {
+  list-style-type: none;
+  font-family: "Playfair Display SC", serif;
+  line-height: 4rem;
+  padding-bottom: 16px;
+  transition: color 0.3s ease;
+  cursor: pointer;
+  opacity: 0;
   border-bottom: 1px solid transparent;
-  transition: all 0.5s ease;
+  animation: slide_up_img 0.9s ease 1.5s forwards;
 }
 
 li:hover {
-  border-bottom: 1px solid white;
-  color: #eaeaea;
+  border-bottom: 1px solid #50ccc7;
+  color: #50ccc7;
 }
 
 header {
   z-index: 1;
 }
 
+.navLogo {
+  z-index: 10;
+  color: #d0e8e2;
+}
+
 /* CART POPUP STYLES */
 .cartImport {
-  position: relative;
-  right: 0;
-  top: 0;
+  z-index: 5;
   transition: all 0.5s ease-in-out;
+}
+
+.fall-down-enter,
+.fall-down-enter-active,
+.fall-down-leave-active {
+  top: -100%;
 }
 
 .slide-enter,
@@ -150,7 +161,61 @@ header {
   right: -100%;
 }
 
-.noUnderline:hover {
-  text-decoration: none !important;
+/* HAMBURGER BAR  */
+
+.hamburger {
+  margin-left: 20px;
+  /* padding: 2px; */
+  padding-top: 6px;
+
+  height: 1.8rem;
+  width: 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  z-index: 3;
+  /* border: 1px solid red; */
+}
+
+.hamburger:hover {
+  cursor: pointer;
+}
+
+.hamburgerOpen {
+  z-index: 10;
+}
+
+.hamburger-line {
+  height: 0.18rem;
+  width: 80%;
+  background: #627f8a;
+  transition: transform 300ms cubic-bezier(0.445, 0.05, 0.55, 0.95),
+    opacity 300ms linear;
+}
+
+.hamburgerOpen .hamburger-line-first {
+  transform: translate(0, 9px) rotate(45deg);
+  background: #d0e8e2;
+  width: 100%;
+}
+
+.hamburgerOpen .hamburger-line-middle {
+  opacity: 0;
+}
+
+.hamburgerOpen .hamburger-line-last {
+  transform: translate(0, -11px) rotate(-45deg);
+  background: #d0e8e2;
+  width: 100%;
+}
+
+@keyframes slide_up_img {
+  0% {
+    transform: translateY(250px);
+  }
+  100% {
+    transform: translateY(0);
+    opacity: 1;
+  }
 }
 </style>
