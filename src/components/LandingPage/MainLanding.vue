e6b170dfc93d252c2b564706f296ab6ced4e0515
EJ McVey <ej.mcvey@geopal-solutions.com>
2021-05-11 20:49:36 +0100

    main landing page revamp

---
+++ src/components/LandingPage/MainLanding.vue
@@ -0,0 +1,148 @@
+<template>
+  <section class="pb-10">
+    <h1 class="text-2xl sm:text-4xl mt-5">Kathryn Ceramics</h1>
+
+    <nav
+      class="hidden sm:mt-20 sm:flex w-3/4 justify-between m-auto text-xs uppercase font-bold"
+    >
+      <div v-for="(link, idx) in navInfo" :key="idx">
+        <li
+          class="pb-3"
+          :class="{ activeClass: link.active }"
+          @click="changeImages(link)"
+        >
+          {{ link.name }}
+        </li>
+      </div>
+    </nav>
+
+    <div class="w-full flex mt-10 sm:mt-4" v-if="showImages == 'favourites'">
+      <img
+        class="imageSlideUp w-1/2 h-96"
+        :src="require(`../../assets/latestDesigns/mug1.jpeg`)"
+      />
+
+      <img
+        class="imageSlideUp2 w-1/2 h-96"
+        :src="require(`../../assets/latestDesigns/misc1.jpeg`)"
+      />
+    </div>
+
+    <div class="w-full flex mt-7" v-if="showImages == 'tableware'">
+      <img
+        class="imageSlideUp w-1/2 h-96"
+        :src="require(`../../assets/latestDesigns/dish1.jpeg`)"
+      />
+
+      <img
+        class="imageSlideUp2 w-1/2 h-96"
+        :src="require(`../../assets/latestDesigns/dish2.jpeg`)"
+      />
+    </div>
+
+    <div class="w-full flex mt-7" v-if="showImages == 'cups'">
+      <img
+        class="imageSlideUp w-1/2 h-96"
+        :src="require(`../../assets/latestDesigns/mug1.jpeg`)"
+      />
+
+      <img
+        class="imageSlideUp2 w-1/2 h-96"
+        :src="require(`../../assets/latestDesigns/mug2.jpeg`)"
+      />
+    </div>
+
+    <div class="w-full flex mt-7" v-if="showImages == 'garden'">
+      <img
+        class="imageSlideUp w-1/2 h-96"
+        :src="require(`../../assets/latestDesigns/misc1.jpeg`)"
+      />
+
+      <img
+        class="imageSlideUp2 w-1/2 h-96"
+        :src="require(`../../assets/latestDesigns/misc1.jpeg`)"
+      />
+    </div>
+
+    <div class="w-full flex mt-7" v-if="showImages == 'misc'">
+      <img
+        class="imageSlideUp w-1/2 h-96"
+        :src="require(`../../assets/latestDesigns/bowl1.jpeg`)"
+      />
+
+      <img
+        class="imageSlideUp2 w-1/2 h-96"
+        :src="require(`../../assets/latestDesigns/bowl2.jpeg`)"
+      />
+    </div>
+
+    <router-link to="/shop">
+      <button class="btn-green btn-lrg mt-10 sm:mt-20">View Products</button>
+    </router-link>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showImages: "favourites",
+      navInfo: [
+        { name: "Favourites", map: "favourites", active: true },
+        { name: "Tableware", map: "tableware", active: false },
+        { name: "Cup Sets", map: "cups", active: false },
+        { name: "Garden Ideas", map: "garden", active: false },
+        { name: "Miscellaneous", map: "misc", active: false },
+      ],
+    };
+  },
+  methods: {
+    changeImages(link) {
+      console.log(link);
+      this.navInfo.forEach((val) =>
+        val.name == link.name ? (val.active = true) : (val.active = false)
+      );
+
+      this.showImages = link.map;
+    },
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  font-family: "Playfair Display SC", serif;
+}
+nav li {
+  list-style-type: none;
+  cursor: pointer;
+  border-bottom: 1px solid white;
+  transition: all 0.2s ease-in;
+}
+li:hover {
+  color: #c4c3c5;
+  border-bottom: 1px solid #c4c3c5;
+}
+
+.activeClass {
+  color: #c4c3c5;
+  border-bottom: 1px solid #c4c3c5;
+}
+
+.imageSlideUp {
+  animation: slide_up_img 0.6s ease;
+}
+
+.imageSlideUp2 {
+  animation: slide_up_img 0.9s ease;
+}
+
+@keyframes slide_up_img {
+  0% {
+    transform: translateY(250px);
+  }
+  100% {
+    transform: translateY(0);
+  }
+}
+</style>
