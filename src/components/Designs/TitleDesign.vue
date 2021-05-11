b18884d7300f2ba681f7848c3e933b29c52041f8
EJ McVey <ej.mcvey@geopal-solutions.com>
2021-05-11 20:49:09 +0100

    title colour changes

--- src/components/Designs/TitleDesign.vue
+++ src/components/Designs/TitleDesign.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="flex items-center w-11/12 justify-center mt-5 mb-7">
-    <div class="w-1/2 sm:w-full bg-green-light h-0.5"></div>
-    <div class="w-4/6 text-sm uppercase text-green-dark">
+    <div class="w-1/2 sm:w-full bg-gray-light h-0.5"></div>
+    <div class="w-4/6 text-sm uppercase text-gray-dark">
       {{ title }}
     </div>
-    <div class="w-1/2 sm:w-full bg-green-light h-0.5"></div>
+    <div class="w-1/2 sm:w-full bg-gray-light h-0.5"></div>
   </div>
 </template>
 
