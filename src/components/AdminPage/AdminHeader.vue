ece97173590df20f010658ae89e90a0bcb713ed7
EJ McVey <ej.mcvey@geopal-solutions.com>
2021-05-13 21:23:54 +0100

    admin header svgs

--- src/components/AdminPage/AdminHeader.vue
+++ src/components/AdminPage/AdminHeader.vue
@@ -3,17 +3,40 @@
     class="w-full bg-green-light flex justify-between px-3 py-4 items-center shadow-2xl"
   >
     <router-link to="/">
-      <img
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-5 w-5 cursor-pointer"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="white"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+        />
+      </svg>
+      <!-- <img
         class="w-5 h-5 cursor-pointer"
         src="https://i.ibb.co/jgk6zTW/house.png"
-      />
+      /> -->
     </router-link>
     <p class="text-white font-bold">Welcome Kathryn!</p>
-    <img
+
+    <svg
       @click="logoutUser()"
+      xmlns="http://www.w3.org/2000/svg"
       class="w-5 h-5 cursor-pointer"
-      src="https://i.ibb.co/HV3Kzt9/close.png"
-    />
+      viewBox="0 0 20 20"
+      fill="white"
+    >
+      <path
+        fill-rule="evenodd"
+        d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z"
+        clip-rule="evenodd"
+      />
+    </svg>
   </header>
 </template>
 
