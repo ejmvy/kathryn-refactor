4f553247d0a3c98bd0fc81a44644415999844e96
EJ McVey <ej.mcvey@geopal-solutions.com>
2021-05-17 19:39:42 +0100

    order view pop up css styles

--- src/components/UserProfilePage/OrderViewPopup.vue
+++ src/components/UserProfilePage/OrderViewPopup.vue
@@ -1,34 +1,111 @@
 <template>
   <div
-    class="popupArea bg-white m-auto w-1/2 flex flex-col fixed py-8 px-16 rounded-xl shadow-xl"
+    class="popupIndex h-screen w-screen fixed top-0 left-0 flex justify-center items-center"
   >
-    <div class="labelxs text-gray-dark mb-5">Your Items</div>
-    <div v-for="(product, idx) in orderList" :key="idx">
-      <div class="w-full flex h-36 items-center justify-around">
-        <div class="w-24 h-28 mr-16">
-          <img class="h-28 w-24" :src="getImageUrl(product._id)" />
-        </div>
-        <div class="flex flex-col items-center justify-between">
-          <div class="flex items-center justify-between w-full pb-4">
-            <div class="text-gray-500">{{ product.name }}</div>
-            <div class="text-xs font-bold">{{ product.price }}</div>
+    <div
+      class="bg-white w-11/12 h-4/5 md:w-2/3 lg:w-1/2 md:h-2/3 flex flex-col rounded-sm"
+    >
+      <div class="w-full flex items-center border-b-4 border-gray-100 p-4">
+        <svg
+          @click="closePopup"
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6 cursor-pointer"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="#C4C3C5"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M15 19l-7-7 7-7"
+          />
+        </svg>
+        <p class="ml-16 labelxs text-gray-dark">
+          {{ orderInfo.orderStatus ? orderInfo.orderStatus : "In Progress.." }}
+        </p>
+      </div>
+
+      <div class="flex flex-col md:flex-row h-5/6">
+        <div
+          class="w-full md:w-2/3 h-full overflow-auto flex flex-col md:border-r-4 border-gray-100"
+        >
+          <div v-for="(product, idx) in orderInfo.products" :key="idx">
+            <div class="w-full flex h-36 items-center justify-center">
+              <div class="w-24 h-28 mr-16">
+                <img class="h-28 w-24" :src="getImageUrl(product._id)" />
+              </div>
+              <div class="flex flex-col items-center justify-between">
+                <div class="flex items-center justify-between w-full pb-4">
+                  <div class="text-gray-500">{{ product.name }}</div>
+                  <div class="text-xs font-bold">{{ product.price }}</div>
+                </div>
+                <div class="text-gray-400 text-xs">{{ product._id }}</div>
+                <div class="flex items-center pt-3">
+                  <div class="text-xs">{{ product.colourSelected }}</div>
+                  <div class="h-4 w-0.5 bg-gray-200 mx-5"></div>
+                  <div class="text-xs">Qty {{ product.qty }}</div>
+                </div>
+              </div>
+            </div>
           </div>
-          <div class="text-gray-400 text-xs">{{ product._id }}</div>
-          <div class="flex items-center pt-3">
-            <div class="text-xs">{{ product.colourSelected }}</div>
-            <div class="h-4 w-0.5 bg-gray-200 mx-5"></div>
-            <div class="text-xs">Qty {{ product.qty }}</div>
+        </div>
+        <div
+          class="w-full md:w-1/3 h-full flex flex-col justify-between border-2 border-red-500"
+        >
+          <div>
+            <div class="flex justify-between w-full border-2 border-green-300">
+              <div class="h-8 w-8"></div>
+              <div class="uppercase text-green-dark font-bold mb-10 pt-5">
+                Order Details
+              </div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-8 w-8 transform rotate-90 relative -top-5 right-0"
+                viewBox="0 0 20 20"
+                fill="#365a69"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M10 18a8 8 0 100-16 8 8 0 000 16zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </div>
+            <div class="px-5">
+              <div class="text-left">
+                <label class="label-grey">Ordered: </label>
+                <p class="my-2 text-sm">
+                  {{ convertDate(orderInfo.orderDate) }}
+                </p>
+              </div>
+              <div class="text-left">
+                <label class="label-grey">Ref Number: </label>
+                <p class="my-2" text-sm>{{ orderInfo.orderRef }}</p>
+              </div>
+              <div class="text-left">
+                <label class="label-grey">Sent: </label>
+                <p class="my-2 text-sm">
+                  {{ convertDate(orderInfo.deliveredDate) }}
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="text-left px-5">
+            <label class="label-grey"> Payment Total: </label>
+            <p class="my-2 text-xl font-bold text-green-light text-right">
+              € {{ orderInfo.cartTotal }}
+            </p>
           </div>
         </div>
       </div>
     </div>
-    <button @click="closePopup" class="btn-green btn-lrg mt-10">Close</button>
   </div>
 </template>
 
 <script>
 export default {
-  props: ["orderList"],
+  props: ["orderInfo"],
 
   methods: {
     closePopup() {
@@ -42,19 +119,17 @@ export default {
         ? product.imageUrlArray[0]
         : "https://i.ibb.co/NCDk0sY/corrupt-Image.png";
     },
-  },
-
-  created() {
-    console.log("ORDERS VIEW");
-    console.log(this.orderList);
+    convertDate(orderDate) {
+      const options = { year: "numeric", month: "long", day: "numeric" };
+      return new Date(orderDate).toLocaleDateString(undefined, options);
+    },
   },
 };
 </script>
 
 <style scoped>
 .popupArea {
-  top: 15%;
+  top: 20%;
   left: 25%;
-  z-index: 2;
 }
 </style>
