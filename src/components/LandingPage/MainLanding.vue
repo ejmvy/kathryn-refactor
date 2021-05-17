05b35c8954f05e6bfd372c68d8fb1993cb228e5b
EJ McVey <ej.mcvey@geopal-solutions.com>
2021-05-17 19:39:26 +0100

    popup animation for main landing

--- src/components/LandingPage/MainLanding.vue
+++ src/components/LandingPage/MainLanding.vue
@@ -1,84 +1,86 @@
 <template>
   <section class="pb-10">
-    <h1 class="text-2xl sm:text-4xl mt-5">Kathryn Ceramics</h1>
+    <h1 class="homeTitle text-2xl sm:text-4xl mt-5">Kathryn Ceramics</h1>
 
-    <nav
-      class="hidden sm:mt-20 sm:flex w-3/4 justify-between m-auto text-xs uppercase font-bold"
-    >
-      <div v-for="(link, idx) in navInfo" :key="idx">
-        <li
-          class="pb-3"
-          :class="{ activeClass: link.active }"
-          @click="changeImages(link)"
-        >
-          {{ link.name }}
-        </li>
+    <div class="mainLanding">
+      <nav
+        class="hidden sm:mt-20 sm:flex w-3/4 justify-between m-auto text-xs uppercase font-bold"
+      >
+        <div v-for="(link, idx) in navInfo" :key="idx">
+          <li
+            class="pb-3"
+            :class="{ activeClass: link.active }"
+            @click="changeImages(link)"
+          >
+            {{ link.name }}
+          </li>
+        </div>
+      </nav>
+
+      <div class="w-full flex mt-10 sm:mt-4" v-if="showImages == 'favourites'">
+        <img
+          class="elementSlideUp w-1/2 h-96"
+          :src="require(`../../assets/latestDesigns/mug1.jpeg`)"
+        />
+
+        <img
+          class="elementSlideUp2 w-1/2 h-96"
+          :src="require(`../../assets/latestDesigns/misc1.jpeg`)"
+        />
       </div>
-    </nav>
 
-    <div class="w-full flex mt-10 sm:mt-4" v-if="showImages == 'favourites'">
-      <img
-        class="elementSlideUp w-1/2 h-96"
-        :src="require(`../../assets/latestDesigns/mug1.jpeg`)"
-      />
+      <div class="w-full flex mt-7" v-if="showImages == 'tableware'">
+        <img
+          class="elementSlideUp w-1/2 h-96"
+          :src="require(`../../assets/latestDesigns/dish1.jpeg`)"
+        />
 
-      <img
-        class="elementSlideUp2 w-1/2 h-96"
-        :src="require(`../../assets/latestDesigns/misc1.jpeg`)"
-      />
+        <img
+          class="elementSlideUp2 w-1/2 h-96"
+          :src="require(`../../assets/latestDesigns/dish2.jpeg`)"
+        />
+      </div>
+
+      <div class="w-full flex mt-7" v-if="showImages == 'cups'">
+        <img
+          class="elementSlideUp w-1/2 h-96"
+          :src="require(`../../assets/latestDesigns/mug1.jpeg`)"
+        />
+
+        <img
+          class="elementSlideUp2 w-1/2 h-96"
+          :src="require(`../../assets/latestDesigns/mug2.jpeg`)"
+        />
+      </div>
+
+      <div class="w-full flex mt-7" v-if="showImages == 'garden'">
+        <img
+          class="elementSlideUp w-1/2 h-96"
+          :src="require(`../../assets/latestDesigns/misc1.jpeg`)"
+        />
+
+        <img
+          class="elementSlideUp2 w-1/2 h-96"
+          :src="require(`../../assets/latestDesigns/misc1.jpeg`)"
+        />
+      </div>
+
+      <div class="w-full flex mt-7" v-if="showImages == 'misc'">
+        <img
+          class="elementSlideUp w-1/2 h-96"
+          :src="require(`../../assets/latestDesigns/bowl1.jpeg`)"
+        />
+
+        <img
+          class="elementSlideUp2 w-1/2 h-96"
+          :src="require(`../../assets/latestDesigns/bowl2.jpeg`)"
+        />
+      </div>
+
+      <router-link to="/shop">
+        <button class="btn-green btn-lrg mt-10 sm:mt-20">View Products</button>
+      </router-link>
     </div>
-
-    <div class="w-full flex mt-7" v-if="showImages == 'tableware'">
-      <img
-        class="elementSlideUp w-1/2 h-96"
-        :src="require(`../../assets/latestDesigns/dish1.jpeg`)"
-      />
-
-      <img
-        class="elementSlideUp2 w-1/2 h-96"
-        :src="require(`../../assets/latestDesigns/dish2.jpeg`)"
-      />
-    </div>
-
-    <div class="w-full flex mt-7" v-if="showImages == 'cups'">
-      <img
-        class="elementSlideUp w-1/2 h-96"
-        :src="require(`../../assets/latestDesigns/mug1.jpeg`)"
-      />
-
-      <img
-        class="elementSlideUp2 w-1/2 h-96"
-        :src="require(`../../assets/latestDesigns/mug2.jpeg`)"
-      />
-    </div>
-
-    <div class="w-full flex mt-7" v-if="showImages == 'garden'">
-      <img
-        class="elementSlideUp w-1/2 h-96"
-        :src="require(`../../assets/latestDesigns/misc1.jpeg`)"
-      />
-
-      <img
-        class="elementSlideUp2 w-1/2 h-96"
-        :src="require(`../../assets/latestDesigns/misc1.jpeg`)"
-      />
-    </div>
-
-    <div class="w-full flex mt-7" v-if="showImages == 'misc'">
-      <img
-        class="elementSlideUp w-1/2 h-96"
-        :src="require(`../../assets/latestDesigns/bowl1.jpeg`)"
-      />
-
-      <img
-        class="elementSlideUp2 w-1/2 h-96"
-        :src="require(`../../assets/latestDesigns/bowl2.jpeg`)"
-      />
-    </div>
-
-    <router-link to="/shop">
-      <button class="btn-green btn-lrg mt-10 sm:mt-20">View Products</button>
-    </router-link>
   </section>
 </template>
 
@@ -127,5 +129,40 @@ li:hover {
 .activeClass {
   color: #c4c3c5;
   border-bottom: 1px solid #c4c3c5;
+}
+
+.homeTitle {
+  animation: slide_up_entrance 5s forwards;
+}
+
+.mainLanding {
+  opacity: 0;
+  animation: slide_up_content 2s forwards 3s;
+}
+
+@keyframes slide_up_content {
+  0% {
+    transform: translateY(100%);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+
+@keyframes slide_up_entrance {
+  0% {
+    opacity: 0;
+    transform: translateY(250px);
+  }
+  10% {
+    opacity: 1;
+  }
+  50% {
+    transform: translateY(250px);
+  }
+  100% {
+    transform: translateY(0);
+  }
 }
 </style>
