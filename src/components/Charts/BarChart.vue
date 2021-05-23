a2c612d8061be64bfe14646bbb963f59bf7b1db9
EJ McVey <ej.mcvey@geopal-solutions.com>
2021-05-23 20:37:58 +0100

    new bar chart comp

---
+++ src/components/Charts/BarChart.vue
@@ -0,0 +1,48 @@
+<script>
+import { defineComponent } from "vue";
+import { Bar } from "vue3-chart-v2";
+
+export default defineComponent({
+  name: "Chart",
+  extends: Bar,
+  props: ["chartData"],
+  data() {
+    return {
+      months: [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ],
+    };
+  },
+
+  mounted() {
+    this.renderChart(
+      {
+        labels:
+          this.chartData.name != "mostPopular"
+            ? this.months
+            : this.chartData.labels,
+        datasets: [
+          {
+            label: "Total per month",
+            backgroundColor: "#627F8A",
+            height: "500px",
+            data: this.chartData.data,
+          },
+        ],
+      },
+      { responsive: true, maintainAspectRatio: false }
+    );
+  },
+});
+</script>
