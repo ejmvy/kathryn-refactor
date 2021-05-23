8522558bd98e71c256720d585aadd62367e56b71
EJ McVey <ej.mcvey@geopal-solutions.com>
2021-05-23 20:38:04 +0100

    new line chart comp

---
+++ src/components/Charts/LineChart.vue
@@ -0,0 +1,39 @@
+<script>
+import { defineComponent } from "vue";
+import { Line } from "vue3-chart-v2";
+
+export default defineComponent({
+  name: "LineChart",
+  extends: Line,
+  props: ["chartData"],
+  mounted() {
+    this.renderChart(
+      {
+        labels: [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+          "August",
+          "September",
+          "October",
+          "November",
+          "December",
+        ],
+        datasets: [
+          {
+            label: "Total per month",
+            backgroundColor: "#627F8A",
+            height: "500px",
+            data: this.chartData.data,
+          },
+        ],
+      },
+      { responsive: true, maintainAspectRatio: false }
+    );
+  },
+});
+</script>
