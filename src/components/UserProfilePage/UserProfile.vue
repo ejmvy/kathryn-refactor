aedd498860d21ac3cbfffc160cd4890bdc047989
EJ McVey <ej.mcvey@geopal-solutions.com>
2021-05-24 21:08:56 +0100

    added check for user logged in

--- src/components/UserProfilePage/UserProfile.vue
+++ src/components/UserProfilePage/UserProfile.vue
@@ -1,67 +1,69 @@
 <template>
-  <div class="mainSection slowShow h-screen">
-    <div class="w-screen h-full">
-      <div
-        class="bg-green-light w-full flex items-center justify-between p-2 md:p-3 shadow-xl"
-      >
-        <router-link to="/">
+  <div>
+    <div v-if="userLoggedIn" class="mainSection slowShow h-screen">
+      <div class="w-screen h-full">
+        <div
+          class="bg-green-light w-full flex items-center justify-between p-2 md:p-3 shadow-xl"
+        >
+          <router-link to="/">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5 cursor-pointer"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="white"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+              />
+            </svg>
+          </router-link>
+          <h5 class="text-white uppercase text-xs md:text-lg">My Account</h5>
           <svg
+            @click="logoutUser()"
             xmlns="http://www.w3.org/2000/svg"
-            class="h-5 w-5 cursor-pointer"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="white"
+            class="w-5 h-5 cursor-pointer"
+            viewBox="0 0 20 20"
+            fill="white"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+              fill-rule="evenodd"
+              d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z"
+              clip-rule="evenodd"
             />
           </svg>
-        </router-link>
-        <h5 class="text-white uppercase text-xs md:text-lg">My Account</h5>
-        <svg
-          @click="logoutUser()"
-          xmlns="http://www.w3.org/2000/svg"
-          class="w-5 h-5 cursor-pointer"
-          viewBox="0 0 20 20"
-          fill="white"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z"
-            clip-rule="evenodd"
-          />
-        </svg>
+        </div>
+
+        <UserAccountMenu
+          v-if="!hideMenu"
+          @userSelect="openMenu"
+          :userName="userDetails.name"
+        ></UserAccountMenu>
+
+        <transition name="fade" mode="out-in">
+          <UserOrders
+            v-if="menu.userOrders"
+            @closeMenu="closeMenuOption"
+          ></UserOrders>
+        </transition>
+
+        <transition name="fade" mode="out-in">
+          <UserMainDetails
+            v-if="menu.userDetails"
+            @closeMenu="closeMenuOption"
+          ></UserMainDetails>
+        </transition>
+
+        <transition name="fade" mode="out-in">
+          <UserAddress
+            v-if="menu.userAddress"
+            @closeMenu="closeMenuOption"
+          ></UserAddress>
+        </transition>
       </div>
-
-      <UserAccountMenu
-        v-if="!hideMenu"
-        @userSelect="openMenu"
-        :userName="userDetails.name"
-      ></UserAccountMenu>
-
-      <transition name="fade" mode="out-in">
-        <UserOrders
-          v-if="menu.userOrders"
-          @closeMenu="closeMenuOption"
-        ></UserOrders>
-      </transition>
-
-      <transition name="fade" mode="out-in">
-        <UserMainDetails
-          v-if="menu.userDetails"
-          @closeMenu="closeMenuOption"
-        ></UserMainDetails>
-      </transition>
-
-      <transition name="fade" mode="out-in">
-        <UserAddress
-          v-if="menu.userAddress"
-          @closeMenu="closeMenuOption"
-        ></UserAddress>
-      </transition>
     </div>
   </div>
 </template>
@@ -111,9 +113,21 @@ export default {
       this.hideMenu = false;
     },
   },
+  computed: {
+    userLoggedIn() {
+      const isLogged = this.$store.getters["isAuthenticated"];
+      return isLogged;
+    },
+  },
   mounted() {
     this.userDetails = this.$store.getters["getUserDetails"];
     this.$store.dispatch("prods/callProductsApi");
+
+    const isLogged = this.$store.getters["isAuthenticated"];
+    if (!isLogged) {
+      console.log("to login2");
+      this.$router.push("/login");
+    }
   },
   components: {
     UserAccountMenu,
