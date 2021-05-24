d13225c4fcf3d7b63dd43c3d6dd0a2900944c724
EJ McVey <ej.mcvey@geopal-solutions.com>
2021-05-24 16:40:04 +0100

    added aos to main js

--- src/main.js
+++ src/main.js
@@ -6,10 +6,15 @@ import axios from "axios";
 import VueAxios from "vue-axios";
 import store from "./store";
 import "./assets/styles/tailwind.css";
-// import { Overlay } from "vuejs-overlay.js";
+
 const emitter = mitt();
 
 const app = createApp(App);
+
+import AOS from "aos";
+import "aos/dist/aos.css";
+app.AOS = new AOS.init();
+// { disable: "phone" }
 
 app.config.globalProperties.emitter = emitter;
 
