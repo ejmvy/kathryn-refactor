3b07e532122159703e6cfd2b445b3474cd319049
EJ McVey <ej.mcvey@geopal-solutions.com>
2021-05-25 20:51:54 +0100

    new pie chart comp

---
+++ src/components/Charts/PieChart.vue
@@ -0,0 +1,33 @@
+<script>
+import { defineComponent } from "vue";
+import { Pie } from "vue3-chart-v2";
+
+export default defineComponent({
+  name: "PieChart",
+  extends: Pie,
+  props: ["pieData"],
+  mounted() {
+    this.renderChart(
+      {
+        labels: this.pieData.labels,
+        datasets: [
+          {
+            label: "Total per month",
+            height: "400px",
+            backgroundColor: [
+              "#627F8A",
+              "#D0E8E2",
+              "#365a69",
+              "#50ccc7",
+              "#C4C3C5",
+            ],
+
+            data: this.pieData.data,
+          },
+        ],
+      },
+      { responsive: true, maintainAspectRatio: false }
+    );
+  },
+});
+</script>
