e5f24fd2c031a8237c9811130aaea8ddeba058dd
EJ McVey <ej.mcvey@geopal-solutions.com>
2021-05-31 20:03:26 +0100

    added new svg base component

---
+++ src/components/Designs/SvgBase.vue
@@ -0,0 +1,24 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    class="h-5 w-5 cursor-pointer"
+    fill="none"
+    viewBox="0 0 24 24"
+    :stroke="svgColour ? svgColour : 'currentColor'"
+  >
+    <path
+      fill-rule="evenodd"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      :d="svg"
+      clip-rule="evenodd"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  props: ["svg", "svgColour"],
+};
+</script>
