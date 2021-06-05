a022824206ca0dee1938ea2deb9dfdd9592fd805
EJ McVey <ej.mcvey@geopal-solutions.com>
2021-06-05 13:56:59 +0100

    changed style for mobile footer

--- src/components/LandingPage/ContactFooter.vue
+++ src/components/LandingPage/ContactFooter.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="bg-black-dark flex flex-col w-full py-5">
-    <div class="flex justify-around items-center">
+    <div
+      class="flex flex-col md:flex-row justify-around items-center md:w-full"
+    >
       <div class="logo">
         <p class="logo text-green-white text-4xl sm:text-5xl">KC</p>
       </div>
-      <div class="text-xs text-left text-gray-light">
+      <div class="text-xs md:text-left mt-8 md:mt-0 text-gray-light">
         <p>+353 85 2577 656</p>
         <p>kathrynokelly@gmail.com</p>
         <p>Dublin, Ireland</p>
       </div>
-      <div class="text-xs text-left text-gray-light">
+      <div
+        class="flex md:flex-col w-full md:w-44 justify-around text-xs md:text-left mt-8 md:mt-0 text-gray-light"
+      >
         <p>@2021 {{ getfooterText ? shortName : longName }}</p>
         <p>Designed by EJ</p>
       </div>
